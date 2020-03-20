78203d2d2e4d2000b49b52165f7cc125cd04ce40
pgoonghang <pgoonghang@yahoo.com>
2020-03-20 14:48:31 -0700

    initial commit

---
+++ number_guessing_game.py
@@ -0,0 +1,50 @@
+#!/Library/Frameworks/Python.framework/Versions/3.8/bin/python3
+
+import random
+import os
+
+# Maximum number to include in guessing game
+max_num = 1_000
+
+# Scratch file location
+scratch_file = "./tally.scratch"
+
+correct_responses = ["あたり", "正しい", "よくできました", "すごいね", "正解", "御名答"]
+incorrect_responses = ["おしい", "残念", "違う", "不正解", "間違い", "誤り", "違います"]
+
+guess, tally = None, 0
+
+def say(str):
+   str = "say -v kyoko " + str
+   os.system(str)
+   return
+
+if os.path.isfile(scratch_file):
+    with open(scratch_file, "r") as f:
+        tally = f.read()
+    tally = int(tally)    
+
+os.system("clear")
+
+input("""This program will help you improve your listening comprehension with Japanese numbers.
+You will hear a number spoken in Japanese, and then be asked to type in the number.
+Type your answer using the traditional western numbers (0-9). Type 'q' to quit.
+
+Make sure your sound is turned on and press ENTER  to begin: """)
+print()
+
+while True:
+    num = random.randint(1, max_num)
+    say(str(num))
+    guess = input("Type your answer or 'q' to quit: ")
+    if guess == "q":
+        with open(scratch_file, "w") as f:
+            f.write(str(tally))
+        break
+    elif int(guess) == num:
+        say(random.choice(correct_responses))
+        tally += 1
+    else:
+        say(random.choice(incorrect_responses))
+        tally = 0
+    print(f"correct number: {num} your guess: {guess} streak: {tally}\n")
